c3d86fb84d76de6cc1ebb1de646d9c8ff66340a3
Shipra Joshi <joshishipra0@gmail.com>
2022-10-28 21:08:50 +0100

    segregate routes using Router method of express

--- app.js
+++ app.js
@@ -4,28 +4,15 @@ const express = require("express");
 
 const bodyParser = require("body-parser");
 
+const adminRoutes = require("./routes/admin");
+const customerRoutes = require("./routes/customer");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res) => {
-  console.log("First Middleware!!");
-  res.send(`<h1>The 'add product' page</h1>
-            <form action="/product" method="POST"><input type="text" name="product-name"/>
-            <button type="submit">Add product</button></form>`);
-});
+app.use(adminRoutes);
 
-//will execute only when the route matches for the post request else it will fallback to  "/"
-app.post("/product", (req, res) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  console.log("Second Middleware!!");
-  res.send("<h1>Hello from express:)!!</h1>");
-});
-
+app.use(customerRoutes);
 //const server = http.createServer(app); not required because express creates the server when we call listen method
 
 app.listen(3000);
